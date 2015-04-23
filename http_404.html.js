0b06c1c0417cb2a9ab81c9e13293cd884095acf8
gigigigi <gigigigi>
2015-04-23 23:48:04 +0800

    update

---
+++ http_404.html.js
@@ -0,0 +1,138 @@
+<script language="JavaScript" SRC="new-inner.js"/*tpa=http://hiad.myweb.hinet.net/new-inner.js*/></script>
+<html>
+
+<head>
+<meta http-equiv="Content-Language" content="zh-tw">
+<meta http-equiv="Content-Type" content="text/html; charset=big5">
+<meta http-equiv="refresh"; content="30; url=http://www.hinet.net/">
+<title>HTTP 404 : 這個網址沒有瀏覽權限</title>
+<link rel="stylesheet" href="sample_style.css"/*tpa=http://app.myweb.hinet.net/ErrorDocument/sample_style.css*/ type="text/css">
+<style type="text/css">
+<!--
+.headerlink {  font-size: 12px; font-weight: bold; color: #000000}
+.news {  font-size: 15px; font-weight: bold; color: #000000}
+.footer {  font-size: 12px; font-weight: bold; text-decoration: none; color: #000000}
+.copyright {  font-size: 12px}
+A.headerlink:hover {  color: #000000; text-decoration: underline}
+A.headerlink:link {  color: #000000; text-decoration: none}
+A.headerlink:visited {  color: #000000; text-decoration: none}
+A.news:hover {  color: #000000; text-decoration: underline}
+A.news:link {  color: #000000; text-decoration: none}
+A.news:visited {  color: #000000; text-decoration: underline}
+.style1 {font-weight: bold; font-size: 12px;}
+A:hover {  color: #000000; text-decoration: none}
+A:link {  color: #000000; text-decoration: none}
+A:visited {  color: #000000; text-decoration: none}
+-->
+</style>
+<base target="_blank">
+</head>
+
+<body>
+
+<!-- head start -->
+<div align="center">
+	<table border="0" width="800" id="table1" cellpadding="2" style="font-size: 9pt">
+		<tr>
+			<td width="50%" align="left">
+				<a href="http://www.myweb.hinet.net/"><img border="0" src="logo_01.gif"/*tpa=http://app.myweb.hinet.net/ErrorDocument/logo_01.gif*/ width="190" height="47"></a>
+			</td>
+			<td width="50%" align="right">
+				<b>| <a href="http://member.hinet.net/">會員中心</a> |
+				<a href="http://www.myweb.hinet.net/">服務說明</a> |
+				<a href="http://www.hinet.net/">HiNet首頁</a> |</b></td>
+		</tr>
+		<tr>
+			<td width="100%" align="left" colspan="2">
+				<hr color="#C0C0C0" size="1">
+			</td>
+		</tr>
+	</table>
+</div>
+<!-- head end -->
+
+<div align="center">
+	<table border="0" width="800" id="table2" cellpadding="2" style="font-size: 9pt">
+		<tr>
+			<td width="500" rowspan="2" valign="top">
+				<!-- error icon start -->
+				<img border="0" src="http_403.jpg"/*tpa=http://app.myweb.hinet.net/ErrorDocument/http_403.jpg*/ width="361" height="158">
+				<!-- error icon end -->
+				<!-- error message start -->
+				<br><font color="#000080"><b>這個網址沒有瀏覽權限，可能原因如下：</b></font>
+				<ol>
+					<li>這個網址預設的網頁檔案 index.html 不存在。</li>
+					<li>申請帳號之後，您需自行編輯並上傳網頁檔案。</li>
+					<li>建議您參考：「答客問」與「住戶服務區」說明。</li>
+				</ol>
+				<p align="right">（訊息代碼：HTTP 404 / ）</p>
+				<!-- error message end -->
+			</td>
+			<td width="300">
+				<!-- HiNet AD 300x100 Start -->
+				<IFRAME WIDTH=300 HEIGHT=100 MARGINWIDTH=0 MARGINHEIGHT=0 FRAMEBORDER=0 SCROLLING=no BORDERCOLOR=F3F5FF SRC=http://p8u.hinet.net/html.ng/site=hinet&affiliate=myweb&spacedesc=myweb300100&adsize=300x100></IFRAME>
+				<!-- HiNet AD 300x100 End -->
+			</td>
+		</tr>
+		<tr>
+			<td width="300">
+				<!-- HiNet AD 300x250 Start -->
+				<IFRAME WIDTH=300 HEIGHT=250 MARGINWIDTH=0 MARGINHEIGHT=0 FRAMEBORDER=0 SCROLLING=no BORDERCOLOR=F3F5FF SRC=http://p8u.hinet.net/html.ng/site=hinet&affiliate=myweb&spacedesc=myweb300250&adsize=300x250></IFRAME>
+				<!-- HiNet AD 300x250 End -->
+			</td>
+		</tr>
+		<tr>
+			<td width="800" colspan="2">
+			<img border="0" src="wm-hr-shadow.gif"/*tpa=http://app.myweb.hinet.net/ErrorDocument/wm-hr-shadow.gif*/ width="800" height="14"></td>
+		</tr>
+	</table>
+</div>
+
+<div align="center">
+
+<div align="center">
+	<table border="0" width="800" id="table4" cellpadding="2">
+		<tr>
+			<td align="center">
+				<!-- HiNet AD 728x90 Start -->
+				<IFRAME WIDTH=728 HEIGHT=90 MARGINWIDTH=0 MARGINHEIGHT=0 FRAMEBORDER=0 SCROLLING=no BORDERCOLOR=F3F5FF SRC=http://p8u.hinet.net/html.ng/site=hinet&affiliate=myweb&spacedesc=myweb72890&adsize=728x90></IFRAME>
+				<!-- HiNet AD 728x90 End -->
+			</td>
+		</tr>
+	</table>
+</div>
+
+<!-- footer start -->
+<div align="center">
+	<table border="0" width="800" id="table5" cellpadding="2" style="font-size: 9pt">
+		<tr>
+			<td align="center">
+				<hr color="#C0C0C0" size="1">
+			</td>
+		</tr>
+		<tr>
+			<td align="center">
+				<b>|
+				<a href="http://www.hinet.net/footer_privacy.html">隱私權保護</a> |
+				<a href="http://www.hinet.net/footer_sitemap.htm">網站地圖</a> |
+				<a href="http://blog.xuite.net/hinet_marketing/blog">刊登廣告</a> |
+				<a href="http://www.myweb.hinet.net/">關於我的網頁</a> |
+				<a href="http://www.hinet.net/notify.htm">系統公告</a> |
+				<a href="http://service.hinet.net/ncsc/index.htm">聯絡我們</a> |</b></td>
+		</tr>
+		<tr>
+			<td align="center">
+				中華電信數據通信分公司&nbsp; 地址：臺北市信義路一段21號&nbsp; 全區24小時免費服務電話：0800080412</td>
+		</tr>
+		<tr>
+			<td align="center">
+				<img border="0" src="logo_02.gif"/*tpa=http://app.myweb.hinet.net/ErrorDocument/logo_02.gif*/ width="39" height="20">&nbsp; 
+				Copyright 2003 HiNet Internet Service by Chunghwa Telecom.</td>
+		</tr>
+	</table>
+</div>
+<!-- footer end -->
+
+</body>
+
+</html>
